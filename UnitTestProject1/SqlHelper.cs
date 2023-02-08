5a67eb56d528ec3619993cbbe74193934d1fe51b
Tuan Duong Manh <dmtuan1995@gmail.com>
2023-02-08 10:02:11 +0700

    Add project files.

---
+++ UnitTestProject1/SqlHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMSTest
+{
+    public static class SqlHelper
+    {// Set the connection, command, and then execute the command with non query.  
+        public static Int32 ExecuteNonQuery(String connectionString, String commandText,
+            CommandType commandType)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    // There're three command types: StoredProcedure, Text, TableDirect. The TableDirect   
+                    // type is only for OLE DB.    
+                    cmd.CommandType = commandType;
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Set the connection, command, and then execute the command and only return one value.  
+        public static Object ExecuteScalar(String connectionString, String commandText,
+            CommandType commandType)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        // Set the connection, command, and then execute the command with query and return the reader.  
+        public static SqlDataReader ExecuteReader(String connectionString, String commandText,
+            CommandType commandType)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType = commandType;
+
+                conn.Open();
+                // When using CommandBehavior.CloseConnection, the connection will be closed when the   
+                // IDataReader is closed.  
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                return reader;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
