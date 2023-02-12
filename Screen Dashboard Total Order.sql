efc45442c2c90003c9ea7e6ac004fc295d06b838
Tran Anh Quan <101349018+QuanITT@users.noreply.github.com>
2023-02-12 22:32:52 +0700

    Update Screen Dashboard Total Order.sql

--- Screen Dashboard Total Order.sql
+++ Screen Dashboard Total Order.sql
@@ -4,3 +4,5 @@ FROM OrderList odl
 WHERE odl.OrderStatus = 'COMPLETED'
   AND odl.OrderedAt BETWEEN '2023-01-01 00:00:00' AND DATEADD(SECOND, -1, '2023-07-27')  
   AND Year(odl.OrderedAt) = Year(GETDATE()) 
+  
+  //Dashboard ( SUM theo So luong ) 
