83d58f01af88f9b11101ebd6da8726e2451fe1c8
Trần Tùng <122069454+sh3nl0ng16@users.noreply.github.com>
2023-02-13 09:18:08 +0700

    Update GetGrowUpPercentageByCountry.sql

--- GetGrowUpPercentageByCountry.sql
+++ GetGrowUpPercentageByCountry.sql
@@ -1,16 +1,16 @@
-SELECT Region,
+SELECT Country,
        Round(100*(sum(CASE
                           WHEN month(OrderedAt) = month(getdate())
                                AND year(OrderedAt) = year(getdate()) THEN TotalPrice
-							  ELSE 0
+                          ELSE 0
                       END) - sum(CASE
                                      WHEN month(OrderedAt) = month(dateadd(MONTH, -1, CURRENT_TIMESTAMP))
                                           AND year(OrderedAt) = year(dateadd(MONTH, -1, CURRENT_TIMESTAMP)) THEN TotalPrice
-										  ELSE 0
+                                     ELSE 0
                                  END)) / sum(CASE
                                                  WHEN month(OrderedAt) = month(dateadd(MONTH, -1, CURRENT_TIMESTAMP))
                                                       AND year(OrderedAt) = year(dateadd(MONTH, -1, CURRENT_TIMESTAMP)) THEN TotalPrice
-													  ELSE 0
-                                             END), 0) as PercentageGrowUp
-FROM OrderList
-GROUP BY Region
+                                                 ELSE 0
+                                             END), 0) AS PercentageGrowUp
+FROM OrderInfo
+GROUP BY Country
