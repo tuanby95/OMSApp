c65dc346e158efc550ccc235288aecfcf3c175c2
VietDang <108107174+VietDangiT@users.noreply.github.com>
2023-02-13 09:23:13 +0700

    Update OrderQuery.sql

--- OrderQuery.sql
+++ OrderQuery.sql
@@ -7,22 +7,22 @@ DECLARE @MaxTablePage FLOAT;
 SET @PageNumber = 1;
 SET @RowsOfPage = 5;
 SELECT @MaxTablePage = COUNT(*)
-						FROM OrderList odr
+						FROM OrderInfo odr
 						WHERE CONVERT(date,odr.OrderedAt) BETWEEN '2023-01-24'
 							AND DATEADD(SECOND, 1, '2023-01-31')
 SET @MaxTablePage = CEILING(@MaxTablePage/@RowsOfPage)
 WHILE @MaxTablePage >= @PageNumber
 BEGIN
-	SELECT CONVERT(date,odl.OrderedAt) AS OrderedDate ,
-			SUM(CASE WHEN odl.OrderStatus IN('COMPLETED') THEN 1 ELSE 0 END )AS NumberOfCompleted
-			,SUM(CASE WHEN odl.OrderStatus IN('FAILED') THEN 1 ELSE 0 END )AS NumberOfFailed
-			,SUM(CASE WHEN odl.OrderStatus IN('RETURN') THEN 1 ELSE 0 END )AS NumberOfReturn
+	SELECT CONVERT(date,odr.OrderedAt) AS OrderedDate ,
+			SUM(CASE WHEN odr.OrderStatus IN('COMPLETED') THEN 1 ELSE 0 END )AS NumberOfCompleted
+			,SUM(CASE WHEN odr.OrderStatus IN('FAILED') THEN 1 ELSE 0 END )AS NumberOfFailed
+			,SUM(CASE WHEN odr.OrderStatus IN('RETURN') THEN 1 ELSE 0 END )AS NumberOfReturn
 			,COUNT(1) AS NumberOfOrder
-	 FROM [OrderList] odl
+	 FROM OrderInfo odr
 	 WHERE
-		CONVERT(date,odl.OrderedAt) BETWEEN '2023-01-24'
+		CONVERT(date,odr.OrderedAt) BETWEEN '2023-01-24'
 		AND DATEADD(SECOND, 1, '2023-01-31')
-	 GROUP BY CONVERT(date,odl.OrderedAt)
+	 GROUP BY CONVERT(date,odr.OrderedAt)
 	 ORDER BY OrderedDate ASC
 	 OFFSET (@PageNumber-1)*@RowsOfPage ROWS FETCH NEXT @RowsOfPage ROWS ONLY
 	 SET @PageNumber = @PageNumber + 1 
