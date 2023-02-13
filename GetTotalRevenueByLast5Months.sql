53b91bd7a9c5f341fa66f102606eba75927c7aad
Trần Tùng <122069454+sh3nl0ng16@users.noreply.github.com>
2023-02-13 09:15:33 +0700

    Update GetTotalRevenueByLast5Months.sql

--- GetTotalRevenueByLast5Months.sql
+++ GetTotalRevenueByLast5Months.sql
@@ -1,11 +1,12 @@
 --1 TotalRevenue by Month
- SELECT 
-	SUM(odl.TotalPrice) AS RevenueCol,
-	DATENAME(MONTH,odl.OrderedAt) as MonthCharCol,
-	YEAR(odl.OrderedAt) as YearCol
-FROM OrderList odl
-WHERE odl.OrderStatus = 'COMPLETED'
-and OrderedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4 ), 0)
-GROUP BY DATENAME(MONTH,odl.OrderedAt), YEAR(odl.OrderedAt),
-		MONTH(odl.OrderedAt)
-ORDER BY YEAR(odl.OrderedAt), MONTH(odl.OrderedAt) ASC;
+SELECT SUM(odi.TotalPrice) AS RevenueCol,
+       DATENAME(MONTH, odi.OrderedAt) AS MonthCharCol,
+       YEAR(odi.OrderedAt) AS YearCol
+FROM OrderInfo AS odi
+WHERE odi.OrderStatus = 'COMPLETED'
+  AND OrderedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4), 0)
+GROUP BY DATENAME(MONTH, odi.OrderedAt),
+         YEAR(odi.OrderedAt),
+         MONTH(odi.OrderedAt)
+ORDER BY YEAR(odi.OrderedAt),
+         MONTH(odi.OrderedAt) ASC;
