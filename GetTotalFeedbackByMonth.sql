cde75b9543d55fbe8c48630fd8e161e5046de630
Trần Tùng <122069454+sh3nl0ng16@users.noreply.github.com>
2023-02-13 09:16:06 +0700

    Update GetTotalFeedbackByMonth.sql

--- GetTotalFeedbackByMonth.sql
+++ GetTotalFeedbackByMonth.sql
@@ -1,7 +1,7 @@
-SELECT count(fdk.Id) AS FeedbackCol,
-       datename(MONTH, fdk.CreatedAt) AS MonthChar,	
+SELECT count(fdk.Id) AS NumberOfFeedback,
+       datename(MONTH, fdk.CreatedAt) AS MonthChar,
        month(fdk.CreatedAt) AS MonthCol
 FROM Feedback AS fdk
-WHERE  fdk.CreatedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4 ), 0)
-	 GROUP BY datename(MONTH, fdk.CreatedAt),
+WHERE fdk.CreatedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4), 0)
+GROUP BY datename(MONTH, fdk.CreatedAt),
          month(fdk.CreatedAt);
