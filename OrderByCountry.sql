ad630c7e02b830c80176ab80e8341c5cbaec81bc
VietDang <108107174+VietDangiT@users.noreply.github.com>
2023-02-13 09:23:44 +0700

    Update OrderByCountry.sql

--- OrderByCountry.sql
+++ OrderByCountry.sql
@@ -1,5 +1,4 @@
-USE OMSDb1
-GO
+
 
 ---		DASHBOARD SCREEN (sale by location [Orders] by location table )
 DECLARE @PageNumber INT;
@@ -8,20 +7,20 @@ DECLARE @MaxTablePage FLOAT;
 SET @PageNumber = 1;
 SET @RowsOfPage = 5;
 SELECT @MaxTablePage = COUNT(*)
-						FROM OrderList odr
+						FROM OrderInfo odr
 						WHERE CONVERT(date,odr.OrderedAt) BETWEEN '2023-01-24'
 							  AND DATEADD(DAY, 1, '2023-01-28')
 SET @MaxTablePage = CEILING(@MaxTablePage/@RowsOfPage)
 WHILE @MaxTablePage >= @PageNumber
 BEGIN
-	SELECT odr.Region
+	SELECT odr.Country
 			,COUNT(odr.Id) AS NumberOfOrders
 			,SUM(odr.TotalPrice) AS TotalSales
-	FROM OrderList odr
+	FROM OrderInfo odr
 	WHERE  odr.OrderStatus NOT IN('FAILED','CANCELLED','RETURN')
 		AND CONVERT(date,odr.OrderedAt) BETWEEN '2023-01-24'
 		AND DATEADD(SECOND, 1, '2023-01-30')
-	GROUP BY odr.Region
+	GROUP BY odr.Country
 	ORDER BY TotalSales DESC
 	OFFSET (@PageNumber-1)*@RowsOfPage ROWS FETCH NEXT @RowsOfPage ROWS ONLY
 	SET @PageNumber = @PageNumber + 1 
