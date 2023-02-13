97ecddb5764dbc694bc35be9d1b5b3b3096d37de
Trần Tùng <122069454+sh3nl0ng16@users.noreply.github.com>
2023-02-13 09:16:47 +0700

    Update GetTotalChannelSaleByMonth.sql

--- GetTotalChannelSaleByMonth.sql
+++ GetTotalChannelSaleByMonth.sql
@@ -1,31 +1,32 @@
 
 --2TotalSaleByMonth
-WITH tmpChannelMonth AS(
-	SELECT MonthValue, c.*
-	FROM 
-	(
-		SELECT (1) AS MonthValue union
-		SELECT 2 union
-		SELECT 3 union
-		SELECT 4 union
-		SELECT 5 union
-		SELECT 6 union
-		SELECT 7 union
-		SELECT 8 union
-		SELECT 9 union
-		SELECT 10 union
-		SELECT 11 union
-		SELECT 12 
-	) t  cross join 
-	Channel as c
-)
-
-SELECT chn.MonthValue, chn.ChannelName, Sum(odl.TotalPrice) Totals, Datename(year,  odl.OrderedAt) as YearCol
-FROM 
-	tmpChannelMonth chn LEFT JOIN
-	OrderList as odl ON odl.ChannelId = chn.Id and chn.MonthValue = Month(odl.OrderedAt)
-WHERE odl.OrderStatus = 'COMPLETED'
-	AND OrderedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4 ), 0)
-Group by chn.MonthValue, chn.ChannelName,Datename(year,  odl.OrderedAt)
-Order by chn.MonthValue;
-
+WITH tmpChannelMonth AS
+  (SELECT MonthValue,
+          c.*
+   FROM
+     (SELECT (1) AS MonthValue
+      UNION SELECT 2
+      UNION SELECT 3
+      UNION SELECT 4
+      UNION SELECT 5
+      UNION SELECT 6
+      UNION SELECT 7
+      UNION SELECT 8
+      UNION SELECT 9
+      UNION SELECT 10
+      UNION SELECT 11
+      UNION SELECT 12) t
+   CROSS JOIN Channel AS c)
+SELECT chn.MonthValue,
+       chn.ChannelName,
+       Sum(odi.TotalPrice) Totals,
+       Datename(YEAR, odi.OrderedAt) AS YearCol
+FROM tmpChannelMonth chn
+LEFT JOIN OrderInfo AS odi ON odi.ChannelId = chn.Id
+AND chn.MonthValue = Month(odi.OrderedAt)
+WHERE odi.OrderStatus = 'COMPLETED'
+  AND OrderedAt > DATEADD(MONTH, (DATEDIFF(MONTH, 0, CURRENT_TIMESTAMP) - 4), 0)
+GROUP BY chn.MonthValue,
+         chn.ChannelName,
+         Datename(YEAR, odi.OrderedAt)
+ORDER BY chn.MonthValue;
